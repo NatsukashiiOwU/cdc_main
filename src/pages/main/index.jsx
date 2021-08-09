9437a91a220a5c7d751500a0c841ade3e595389b
Dima Kyznetsov <zam.dimas240@gmail.com>
2021-08-09 12:17:55 +0000

    95978 - Код-классы | Реализация 2 и 3 секции главной страницы

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,10 +1,16 @@
 import React, { memo } from 'react';
+import Tasks from '../../components/mainPage/Tasks/index';
+import AboutAs from '../../components/mainPage/AboutAs/index';
 import Events from '../../components/mainPage/Events';
 
+import styles from './main.scss';
+
 const MainPage = () => (
-  <>
+  <div className={styles.mainPage}>
+    <Tasks />
+    <AboutAs />
     <Events />
-  </>
+  </div>
 );
 
 export default memo(MainPage);
