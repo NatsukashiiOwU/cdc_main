7c07b80b7b7bc10888b87b46bf490e49a3f65adc
Damir Volkov <voldam256@mail.ru>
2021-08-11 17:31:21 +0000

    Код-классы | Реализация первой секции главной страницы

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 
+import Basics from '../../components/Basics';
 import Events from '../../components/mainPage/Events';
 import Tasks from '../../components/mainPage/Tasks/index';
 import AboutAs from '../../components/mainPage/AboutAs/index';
@@ -8,6 +9,7 @@ import styles from './main.scss';
 
 const MainPage = () => (
   <div className={styles.mainPage}>
+    <Basics />
     <Tasks />
     <AboutAs />
     <Events />
