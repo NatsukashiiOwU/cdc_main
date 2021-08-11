2877ae1b70d19b2445f3b69ce5d76e933995f6f5
Dima Kyznetsov <zam.dimas240@gmail.com>
2021-08-11 14:07:18 +0000

    96402 - Код-классы | Реализация секции карты главной страницы

---
+++ src/components/listPage/CodeClass/index.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import React from 'react-dom';
+import Button from '../../UI/Button';
+import Sprite from '../../../assets/sprites/icons.svg';
+import styles from './CodeClass.module.scss';
+
+import buttonImage from '../../../assets/images/listPage/CodeClass/buttomImage.png';
+
+const CodeClass = ({
+  imageWebp,
+  imagePng,
+  title,
+  color,
+  personImageWebp,
+  personImagePng,
+  personSurname,
+  personName,
+}) => {
+
+  return (
+    <div className={styles.codeClass}>
+      <div className={styles.codeClass__image}>
+        {color ? (
+          <div className={styles.codeClass__svg} style={{ backgroundColor: color, width: '100%', height: '100%' }}>
+            <svg>
+              <use href={`${Sprite}#classes`} />
+            </svg>
+          </div>
+        ) : (
+          <picture>
+            <source srcSet={imageWebp} type="image/webp" />
+            <img src={imagePng} alt={title} />
+          </picture>
+        )}
+
+      </div>
+      <div className={styles.codeClass__description}>
+        <div className={styles.codeClass__title}>
+          <h3>{title}</h3>
+        </div>
+        <div className={styles.codeClass__info}>
+          <div className={styles.codeClass__person}>
+            <div className={styles.codeClass__personImage}>
+              <picture>
+                <source srcSet={personImageWebp} type="image/webp" />
+                <img src={personImagePng} alt={title} />
+              </picture>
+            </div>
+            <div className={styles.codeClass__personName}>
+              <div>{personSurname}</div>
+              <div>{personName}</div>
+            </div>
+          </div>
+          <div className={styles.codeClass__buttons}>
+            <Button classname={styles.codeClass__button} title="ПОДРОБНЕЕ" />
+            <Button classname={styles.codeClass__buttonContextMenu} icon={buttonImage} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodeClass;
