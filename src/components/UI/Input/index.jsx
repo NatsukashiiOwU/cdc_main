569c4d26c63e817ca7d2c26530789a3687789fbe
Дима <zam.dimas240@gmail.com>
2021-09-03 16:50:24 +0400

    fix main page

--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -28,29 +28,31 @@ const Input = ({
         value={text}
       />
     ) : (
-      <label htmlFor={name} className={labelClassName}>
-        {!text ? filePlaceholder : null}
-        <input
-          {...props}
-          id={name}
-          type={type}
-          className={cn(styles.input, !text && styles.input_error, className)}
-          onChange={onChange}
-          name={name}
-          value={text}
-        />
-      </label>
-    )}
-    <div className={cn(styles.input__placeholder,
-      !!text && styles.input__placeholder_none)}
-    >
-      <div className={styles.input__content}>
-        <Icon className={styles.input__icon} view={icon} />
-        <div className={cn(styles.input__placeholder_text, classNamePlaceholder)}>
-          {placeholder}
+      <div className={styles.input__placeholderBlock}>
+        <label htmlFor={name} className={labelClassName}>
+          {!text ? filePlaceholder : null}
+          <input
+            {...props}
+            id={name}
+            type={type}
+            className={cn(styles.input, !text && styles.input_error, className)}
+            onChange={onChange}
+            name={name}
+            value={text}
+          />
+        </label>
+        <div className={cn(styles.input__placeholder,
+          !!text && styles.input__placeholder_none)}
+        >
+          <div className={styles.input__content}>
+            <Icon className={styles.input__icon} view={icon} />
+            <div className={cn(styles.input__placeholder_text, classNamePlaceholder)}>
+              {placeholder}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    )}
   </>
 );
 
